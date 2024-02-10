e0dd099fe04b461319012df78ecb9f7e60759b02
RAHUL-KAD <kadamrahulj0909@gmail.com>
2024-02-10 22:47:31 +0530

    added left space

--- pages/tools/email-validation.tsx
+++ pages/tools/email-validation.tsx
@@ -94,7 +94,7 @@ export default function EmailValidation() {
                         </div>
                         <button
                             type="submit"
-                            className="inline-flex lg:ml-10 items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            className="inline-flex ml-10 items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                         >
                             Verify
                         </button>
