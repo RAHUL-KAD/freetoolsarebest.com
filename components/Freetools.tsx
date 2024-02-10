b54d9908e4f27e57940df4fc9961355c147ae5bf
RAHUL-KAD <kadamrahulj0909@gmail.com>
2024-02-10 15:58:33 +0530

    added landing pages for few tools

--- components/Freetools.tsx
+++ components/Freetools.tsx
@@ -33,11 +33,12 @@ export default function Freetools() {
 
                     
                   </div>
-                  <button
+                  <a
+                      href={`tools/${episod.url}`}
                       className="me-2 lg:ml-5 ml-2 md:ml-5 w-36 lg:mt-5 sm:mt-2 md:mt-2 mt-1 mb-2 rounded-lg border border-blue-700 px-2 py-1 text-center text-sm font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800"
                     >
                       {episod.button}
-                    </button>
+                    </a>
                 </div>
               </Suspense>
             </>
---
+++ pages/tools/email-validation.tsx
@@ -0,0 +1,59 @@
+import Head from "next/head";
+import Header from "../../components/Header";
+
+// https://mailbite.io/
+// https://listclean.xyz/
+
+export default function EmailValidation() {
+    return (
+
+        <div className="w-[100%] flex flex-col items-center justify-center py-2">
+            <Head>
+                <title>freetoolsarebest | Use all the tools for free </title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <svg
+                className="absolute inset-0 -z-10 h-full w-full stroke-green-100 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+                aria-hidden="true"
+            >
+                <defs>
+                    <pattern
+                        id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+                        width="200"
+                        height="200"
+                        x="50%"
+                        y="-1"
+                        patternUnits="userSpaceOnUse"
+                    >
+                        <path d="M.5 200V.5H200" fill="none" />
+                    </pattern>
+                </defs>
+                <rect
+                    width="100%"
+                    height="100%"
+                    stroke-width="0"
+                    fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
+                />
+            </svg>
+
+            <Header />
+
+            <main className="flex justify-center items-center px-4">
+                <div className="mt-10 sm:mt-10">
+                    {/* <Badge text={"Try our GenAI solution for Contact Center"} /> */}
+
+                    <h1 className="font-display mx-auto max-w-3xl text-center text-5xl font-bold tracking-normal text-slate-900 sm:text-5xl">
+                        <span className="relative text-[#333] whitespace-wrap">
+                            <span className="relative mr-2">
+                                Fast & Accurate Free Email Verification Service
+                            </span>
+                        </span>
+                    </h1>
+                </div>
+            </main>
+
+        </div>
+    )
+
+}
