5b58382fd340d372968ac75fbb1290334484ed7e
RAHUL-KAD <kadamrahulj0909@gmail.com>
2024-02-13 21:06:46 +0530

    added upload button

--- pages/tools/email-validation.tsx
+++ pages/tools/email-validation.tsx
@@ -93,12 +93,12 @@ export default function EmailValidation() {
                     </h1>
                 </div>
 
-                <div className="mt-10 ">
+                <div className="mt-10 flex items-center justify-center flex-col">
                     <form className="flex items-center max-w-[600px]" onSubmit={handleSubmit}>
                         <div className="relative">
                             <input
                                 type="email"
-                                className="bg-gray-50 lg:w-[28rem] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                className="bg-gray-50 lg:w-[28rem] md:w-[20rem] border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Enter your email to test..."
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
@@ -137,6 +137,40 @@ export default function EmailValidation() {
                             </pre> */}
                         </div>
                     )}
+
+                    <div className="mr-10">
+                        <p className="mt-10 text-center text-[#333] whitespace-wrap">or you can upload <span className="font-bold"> CSV</span> containing email address  </p>
+
+                        <form  className='mt-4'>
+
+                            <div className="mt-10 flex items-center justify-center">
+                                <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center lg:w-[28rem] w-full h-32 border-2 border-blue-700 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                        </svg>
+                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span></p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400">Upload a CSV file. (File sholud be less than 30 MB) </p>
+                                    </div>
+
+                                    <input type="file" id="dropzone-file" className="hidden" />
+
+                                </label>
+                            </div>
+
+                            
+
+                                <div className='flex items-center justify-center flex-col'>
+
+                                    
+                                    {/* <button type="button" className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Light</button> */}
+                                    {/* <button className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button> */}
+
+                                    <button className="mt-10 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-10 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"> Submit</button>
+
+                                </div>
+                        </form>
+                    </div>
                 </div>
 
             </main>
